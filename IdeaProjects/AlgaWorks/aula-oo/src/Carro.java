c232f16c59ea834df20b67f5437c147a984fec09
Juliana Andrade <juliana.andraden07@gmail.com>
2023-09-22 15:26:02 -0300

    Criando e invocando um método

--- IdeaProjects/AlgaWorks/aula-oo/src/Carro.java
+++ IdeaProjects/AlgaWorks/aula-oo/src/Carro.java
@@ -4,4 +4,9 @@ public class Carro {
     String modelo;
     String cor;
     int anoFabricacao;
+    Pessoa proprietaria;
+
+    void calcularValorRevenda() {
+        System.out.printf("Calcular valor revenda de: %s %d%n", modelo, anoFabricacao);
+    }
 }
--- IdeaProjects/AlgaWorks/aula-oo/src/Principal.java
+++ IdeaProjects/AlgaWorks/aula-oo/src/Principal.java
@@ -1,28 +1,45 @@
 public class Principal {
 
     public static void main(String[] args) {
+        Pessoa eu = new Pessoa();
+        eu.nome = "Juliana";
+        eu.cpf = "123.456.789.01";
+        eu.anoNascimento = 1999;
+
+        Pessoa voce = new Pessoa();
+        voce.nome = "Ju";
+        voce.cpf = "987.654.321-09";
+        voce.anoNascimento = 1998;
+
         Carro meuCarro = new Carro();
         meuCarro.anoFabricacao = 2021;
         meuCarro.cor = "Azul";
         meuCarro.fabricante = "Honda";
         meuCarro.modelo = "HR-V";
+        meuCarro.proprietaria = eu;
 
         Carro seuCarro = new Carro();
         seuCarro.anoFabricacao = 2022;
         seuCarro.cor = "Branco";
         seuCarro.fabricante = "BMW";
         seuCarro.modelo = "X6";
+        seuCarro.proprietaria = voce;
 
-        System.out.println("---------");
-        System.out.println("Meu carro");
-        System.out.printf("Modelo: %s%n", meuCarro.modelo);
-        System.out.printf("Ano: %d%n", meuCarro.anoFabricacao);
+        meuCarro.calcularValorRevenda();
+        seuCarro.calcularValorRevenda();
 
-        System.out.println();
-
-        System.out.println("---------");
-        System.out.println("Seu carro");
-        System.out.printf("Modelo: %s%n", seuCarro.modelo);
-        System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
+//        System.out.println("---------");
+//        System.out.println("Meu carro");
+//        System.out.printf("Modelo: %s%n", meuCarro.modelo);
+//        System.out.printf("Ano: %d%n", meuCarro.anoFabricacao);
+//        System.out.printf("Proprietária: %s%n", meuCarro.proprietaria.nome);
+//
+//        System.out.println();
+//
+//        System.out.println("---------");
+//        System.out.println("Seu carro");
+//        System.out.printf("Modelo: %s%n", seuCarro.modelo);
+//        System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
+//        System.out.printf("Proprietária: %s%n", seuCarro.proprietaria.nome);
     }
 }
