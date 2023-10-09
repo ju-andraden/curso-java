5e2f16ddd57f27531ad32b28d15ab86ce361676d
Juliana Andrade <juliana.andraden07@gmail.com>
2023-10-09 17:36:52 -0300

    Renomeando nome da classe Paciente para CalculadoraImc para alterar sua responsabilidade

--- IdeaProjects/AlgaWorks/calculadora-imc/src/Principal.java
+++ IdeaProjects/AlgaWorks/calculadora-imc/src/Principal.java
@@ -1,11 +1,11 @@
 public class Principal {
 
     public static void main(String[] args) {
-        Paciente paciente = new Paciente();
-        paciente.altura = 1.62;
-        paciente.peso = 71.5;
+        CalculadoraImc calculadoraImc = new CalculadoraImc();
+        calculadoraImc.altura = 1.62;
+        calculadoraImc.peso = 71.5;
 
-        IndiceMassaCorporal imc = paciente.calcularIndicieMassaCorporal();
+        IndiceMassaCorporal imc = calculadoraImc.calcular();
 
         if (imc.estaComObesidade()) {
         System.out.printf("Paciente com altura de %.2f e peso de %.2f " +
