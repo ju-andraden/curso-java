9933f9f55e6afc7e2a418d3c3d2d9c2acbad1939
Juliana Andrade <juliana.andraden07@gmail.com>
2023-10-09 15:57:49 -0300

    Saindo do método com a cláusula return

--- IdeaProjects/AlgaWorks/aula-oo/src/Carro.java
+++ IdeaProjects/AlgaWorks/aula-oo/src/Carro.java
@@ -11,22 +11,36 @@ public class Carro {
         double valorRevendaMeuCarro = calcularValorRevenda();
         int tempoDeUsoMeuCarro = calcularTempoDeUsoEmAnos();
 
+        if (precoCompra <= 0) {
+            System.out.println("Carro com preço de compra zerado." +
+                    "Não foi possível imprimir o resumo de depreciação.");
+
+            return;
+        }
+
         System.out.printf("Tempo de uso (anos): %d%n", tempoDeUsoMeuCarro);
         System.out.printf("Valor de revenda: %6.2f%n", valorRevendaMeuCarro);
     }
 
     double calcularIpva() {
+        int tempoDeUsoEmAnos = calcularTempoDeUsoEmAnos();
+
+        if (tempoDeUsoEmAnos >= 10) {
+            return 0;
+        }
+
         return calcularValorRevenda() * 0.04;
     }
 
     int calcularTempoDeUsoEmAnos() {
-        return 2022 - anoFabricacao;
+        return 2023 - anoFabricacao;
     }
     double calcularValorRevenda() {
         int tempoDeUsoEmAnos = calcularTempoDeUsoEmAnos();
         int vidaUtilEmAnos = 20;
 
-        double valorRevenda = (precoCompra / vidaUtilEmAnos) * (vidaUtilEmAnos - tempoDeUsoEmAnos);
+        double valorRevenda = (precoCompra / vidaUtilEmAnos)
+                * (vidaUtilEmAnos - tempoDeUsoEmAnos);
 
         if (valorRevenda < 0) {
             valorRevenda = 0;
