7acbbf9fa6da694ce03dc7215a914472ca95526e
Juliana Andrade <juliana.andraden07@gmail.com>
2023-10-18 14:13:00 -0300

    maior-numero: sobrecarga de métodos

---
+++ IdeaProjects/AlgaWorks/maior-numero/src/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println(NumeroUtil.maiorNumero(6, 5));
+        System.out.println(NumeroUtil.maiorNumero(6,5,4));
+        System.out.println(NumeroUtil.maiorNumero(10.5, 9.5));
+        System.out.println(NumeroUtil.maiorNumero(11.5, 10.5, 9.5));
+    }
+}
