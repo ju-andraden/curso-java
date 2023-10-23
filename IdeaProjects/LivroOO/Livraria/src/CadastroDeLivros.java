40b62978c2a4deade8aaef1b00122d4b08034b95
Juliana Andrade <juliana.andraden07@gmail.com>
2023-10-23 15:04:51 -0300

    Adicionando o método mostrarDetalhes

--- IdeaProjects/LivroOO/Livraria/src/CadastroDeLivros.java
+++ IdeaProjects/LivroOO/Livraria/src/CadastroDeLivros.java
@@ -6,10 +6,7 @@ public class CadastroDeLivros {
         livro.valor = 59.90;
         livro.isbn = "978-85-66250-46-6";
 
-        System.out.println(livro.nome);
-        System.out.println(livro.descricao);
-        System.out.println(livro.valor);
-        System.out.println(livro.isbn);
+        livro.mostrarDetalhes();
 
         Livro outroLivro = new Livro();
         outroLivro.nome = "Lógica de Programação";
@@ -17,9 +14,6 @@ public class CadastroDeLivros {
         outroLivro.valor = 59.90;
         outroLivro.isbn = "978-85-66250-22-0";
 
-        System.out.println(outroLivro.nome);
-        System.out.println(outroLivro.descricao);
-        System.out.println(outroLivro.valor);
-        System.out.println(outroLivro.isbn);
+        outroLivro.mostrarDetalhes();
     }
 }
--- IdeaProjects/LivroOO/Livraria/src/Livro.java
+++ IdeaProjects/LivroOO/Livraria/src/Livro.java
@@ -3,4 +3,13 @@ public class Livro {
     String descricao;
     double valor;
     String isbn;
+
+    void mostrarDetalhes() {
+        System.out.println("Mostrando detalhes do livro: ");
+        System.out.println("Nome: " + nome);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Valor: " + valor);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("----------");
+    }
 }
